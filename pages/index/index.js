56d535dc3e9fe70aa634cc452fe97f1e353ce107
block1b <2427364631@qq.com>
2019-05-08 17:32:49 +0800

    mqtt,keypair

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,7 @@
 //index.js
 //获取应用实例
 const app = getApp()
+var MQTT = require("../../utils/paho-mqtt.js");
 
 Page({
   data: {
@@ -9,13 +10,7 @@ Page({
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
-  bindSecretKey: function(){
-    console.log('获取密钥');
-    // 在本地用户文件目录下创建一个文件 hello.txt，写入内容
-    const fs = wx.getFileSystemManager()
-    fs.writeFileSync(`${wx.env.USER_DATA_PATH}/secretKey.txt`, '{"public_key":"1","private_key":"a"}', 'utf8');
-    console.log(fs.readFileSync(`${ wx.env.USER_DATA_PATH }/secretKey.txt`,'utf8'));
-  },
+  
   //事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
@@ -23,16 +18,30 @@ Page({
     })
   },
   bindMain:function(){
+    // console.log("user info: " + app.globalData.userInfo.nickName)
+    if (app.globalData.mqtt_client && app.globalData.mqtt_client.isConnected()){
+      console.log("allready connected");
+    }else{
+      console.log("new connect");
+      if (app.globalData.userInfo.nickName) {
+        // 连接mqtt broker emmm 异步延迟 
+        this.mqtt_connect(app.globalData.userInfo.nickName);
+      }
+    }
+    
     wx.navigateTo({
       url: '../main/main'
     })
+  },
+  onReady: function () {
+    
   },
   onLoad: function () {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
-      })
+      });
     } else if (this.data.canIUse){
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
@@ -54,6 +63,7 @@ Page({
         }
       })
     }
+  
   },
   getUserInfo: function(e) {
     console.log(e)
@@ -62,5 +72,61 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
-  }
+  },
+  saveKeyPair: function (text) {
+    // 在本地用户文件目录下创建一个文件，写入内容
+    console.log("保存密钥:" + text);
+    const fs = wx.getFileSystemManager();  // 文件对象
+    fs.writeFileSync(`${wx.env.USER_DATA_PATH}/secretKey.txt`, text, 'utf8');
+  },
+
+  mqtt_connect: function (clientId) {
+    var client = new MQTT.Client("ws://192.168.113.6:8083/mqtt", clientId);
+    var that = this;
+    //connect to  MQTT broker
+    var connectOptions = {
+      timeout: 10,
+      useSSL: false,
+      cleanSession: true,
+      keepAliveInterval: 30,
+      reconnect: true,
+      onSuccess: function () {
+        console.log('connected');
+        app.globalData.mqtt_client = client;
+        client.onMessageArrived = function (msg) {
+          // 路由
+          console.log("receive topic :"+msg.topic+"payload: "+msg.payloadString);
+          var repTopic1 = app.globalData.userInfo.nickName + "/secretKey";
+          if (msg.topic == repTopic1){
+            // 保存密钥到本地文件
+            var text = msg.payloadString;
+            console.log("保存密钥:" + text);
+            const fs = wx.getFileSystemManager();  // 文件对象
+            fs.writeFileSync(`${wx.env.USER_DATA_PATH}/secretKey.txt`, text, 'utf8');
+            // un sub
+            if (app.globalData.mqtt_client && app.globalData.mqtt_client.isConnected()) {
+              app.globalData.mqtt_client.unsubscribe(repTopic1, {
+                onSuccess: function () {
+                  console.log("unsub success");
+                },
+                onFailure: function () {
+                 console.log("unsub err");
+                },
+              });
+            }
+          }
+        };
+
+        client.onConnectionLost = function (responseObject) {
+          if (typeof app.globalData.onConnectionLost === 'function') {
+            return app.globalData.onConnectionLost(responseObject);
+          }
+          if (responseObject.errorCode !== 0) {
+            console.log("onConnectionLost:" + responseObject.errorMessage);
+          }
+        };
+      },
+    };
+    client.connect(connectOptions);
+  },
 })
